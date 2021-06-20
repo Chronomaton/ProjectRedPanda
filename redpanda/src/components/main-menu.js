f84c4c2fe9a47e9d43ca2d9e3b78afc5842eda9b
Nick Fratto <nfratto3@gatech.edu>
2021-06-19 22:33:03 -0400

    vertical align buttons and new font

--- redpanda/src/components/main-menu.js
+++ redpanda/src/components/main-menu.js
@@ -1,12 +1,19 @@
+import Button from '@material-ui/core/Button';
 
 function MainMenu() {
     return (
         <div className="MainMenu">
             <h1>Project Red Panda</h1>
 
-            <button disabled>continue</button>
-            <button onclick="newGame()">new game</button>
-            <button disabled>options</button>
+            <div>
+                <Button variant="contained" disabled >continue</Button>
+            </div>
+            <div>
+                <Button variant="contained" onclick="newGame()">new game</Button>
+            </div>
+            <div>
+                <Button variant="contained" disabled>options</Button>
+            </div>
         </div>
     );
 }
