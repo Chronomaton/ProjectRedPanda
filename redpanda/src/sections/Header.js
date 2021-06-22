6c7442f8426efac6f4d7775ebca850ec72dbbf63
Nick Fratto <nfratto3@gatech.edu>
2021-06-21 21:41:25 -0400

    brought back appbar but made it static

--- redpanda/src/sections/Header.js
+++ redpanda/src/sections/Header.js
@@ -3,16 +3,16 @@ import { AppBar, Toolbar, ButtonGroup } from '@material-ui/core';
 
 function Header() {
     return (
-        // <AppBar color="primary" elevation={0}>
+        <AppBar color="primary" elevation={0} position="static">
             <Toolbar style={{minHeight: "5vh"}}>
-                <ButtonGroup variant="text" color="primary">
+                <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
                     <HeaderButton name="Shipyard" />
                     <HeaderButton name="Factory" />
                     <HeaderButton name="Fleet" />
                     <HeaderButton name="Map" />
                 </ButtonGroup>
             </Toolbar>
-        // </AppBar>
+        </AppBar>
     );
 }
 
