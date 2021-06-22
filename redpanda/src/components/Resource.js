1b20ce13911fbaa1a0d1240e610b8043fe42603e
Chronomaton <dambo1999@gmail.com>
2021-06-21 21:29:34 -0400

    changed to card for resources and added resources to left side on ui

--- redpanda/src/components/Resource.js
+++ redpanda/src/components/Resource.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ResourceModel from '../models/Resource'
 import ResourceList from '../models/ResourceList.json'
 import Button from '@material-ui/core/Button';
-
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { CardActions } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 
 class Resource extends React.Component {
@@ -31,12 +34,16 @@ class Resource extends React.Component {
       <div>
         {this.state.resources.map((item, index) => {
           return(
-            <div key={item.name + "-mining-" + index}>
-              <li>{item.name}</li>
-              <li>Value: {item.value}</li>
-              <li>difficulty: {item.difficulty}</li>
-              <Button onClick={() => this.props.addToInventory(item)}> Mine </Button>
-            </div>
+            <Card key={item.name + "-mining-" + index}>
+              <CardContent>
+                <Typography variant="h5" component="h2">{item.name}</Typography>
+                <Typography variant="body2" component="p">value: {item.value}</Typography>
+                <Typography variant="body2" component="p">difficulty: {item.difficulty}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button onClick={() => this.props.addToInventory(item)}>Send Expedition</Button>
+              </CardActions>
+            </Card>
           )
         })}
       </div>
