9c2b30c8f6368e517c3d767bc2d012b8630f0cd8
Nick Fratto <nfratto3@gatech.edu>
2021-06-27 00:25:08 -0400

    header buttons now more programmatic

--- redpanda/src/sections/Header.js
+++ redpanda/src/sections/Header.js
@@ -2,16 +2,29 @@ import React from "react";
 import HeaderButton from "../components/HeaderButton";
 import { AppBar, Toolbar, ButtonGroup } from '@material-ui/core';
 
+const headerNames = ['Shipyard', 'Factory', 'Fleet', 'Map'];
+
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonList: []
+        }
+        this.getHeaderButtons();
+    }
+
+    getHeaderButtons() {
+        headerNames.forEach((header, i) => {
+            this.state.buttonList.push( <HeaderButton name={header} onChange={this.props.onChange}/>)
+        });
+    }
+
     render() {
         return (
             <AppBar color="primary" elevation={0} position="static">
                 <Toolbar style={{minHeight: "5vh"}}>
                     <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
-                        <HeaderButton name="Shipyard" onChange={this.props.onChange}/>
-                        <HeaderButton name="Factory" onChange={this.props.onChange}/>
-                        <HeaderButton name="Fleet" onChange={this.props.onChange}/>
-                        <HeaderButton name="Map" onChange={this.props.onChange}/>
+                        {this.state.buttonList}
                     </ButtonGroup>
                 </Toolbar>
             </AppBar>
